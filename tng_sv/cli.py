37ab963f479b9c3b4d5aa2af265c1b6a72f78cb3
Lukas Klinger <lukas.klinger9@kit.edu>
2022-11-21 12:34:24 +0100

    only download when necessary

--- tng_sv/cli.py
+++ tng_sv/cli.py
@@ -9,7 +9,7 @@ import numpy as np
 import typer
 
 from tng_sv.api.download import download_snapshot, get_snapshot_amount
-from tng_sv.data.dir import get_delaunay_time_symlink_path, path_to_resampled_file
+from tng_sv.data.dir import get_delaunay_time_symlink_path, get_snapshot_index_path, path_to_resampled_file
 from tng_sv.data.field_type import FieldType
 from tng_sv.data.utils import combine_snapshot, create_delaunay_symlink
 from tng_sv.preprocessing import run_delaunay, run_resample_delaunay
@@ -68,8 +68,11 @@ def run(
 def _run(simulation: Tuple[str, int, FieldType]) -> None:
     """Do things, exit early if already done."""
     simulation_name, snapshot_idx, field_type = simulation
-    if path_to_resampled_file(simulation_name, snapshot_idx, field_type) is None:
+
+    if len(list(get_snapshot_index_path(simulation_name, snapshot_idx).glob("*.*.hdf5"))) == 0:
         download_snapshot(simulation_name, snapshot_idx)
+
+    if path_to_resampled_file(simulation_name, snapshot_idx, field_type) is None:
         combine_snapshot(simulation_name, snapshot_idx, field_type)
         run_delaunay(simulation_name, snapshot_idx, field_type)
         resample(simulation_name, snapshot_idx, field_type)
