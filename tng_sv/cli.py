117d315445bbc6245222b3c17305cbe7f4cbb7f1
buermarc <buermarc2@gmail.com>
2022-11-23 17:58:05 +0100

    Forward exception within pipeline

--- tng_sv/cli.py
+++ tng_sv/cli.py
@@ -1,6 +1,7 @@
 """CLI entrypoint."""
 
 
+import logging
 import os
 from concurrent.futures import ProcessPoolExecutor
 from typing import Tuple, cast
@@ -18,6 +19,8 @@ from tng_sv.data.dir import (
 from tng_sv.data.field_type import FieldType
 from tng_sv.data.utils import combine_snapshot, create_delaunay_symlink, create_resampled_delaunay_symlink
 from tng_sv.preprocessing import run_delaunay, run_resample_delaunay
+
+logger = logging.getLogger(__name__)
 
 app = typer.Typer()
 
@@ -73,21 +76,25 @@ def run(
 
 def _run(simulation: Tuple[str, int, FieldType, bool]) -> None:
     """Do things, exit early if already done."""
-    simulation_name, snapshot_idx, field_type, download = simulation
+    try:
+        simulation_name, snapshot_idx, field_type, download = simulation
 
-    if len(list(get_snapshot_index_path(simulation_name, snapshot_idx).glob("*.*.hdf5"))) == 0 and download:
-        download_snapshot(simulation_name, snapshot_idx)
+        if len(list(get_snapshot_index_path(simulation_name, snapshot_idx).glob("*.*.hdf5"))) == 0 and download:
+            download_snapshot(simulation_name, snapshot_idx)
 
-    if not get_resampled_delaunay_path(simulation_name, snapshot_idx, field_type).exists():
-        combine_snapshot(simulation_name, snapshot_idx, field_type)
-        run_delaunay(simulation_name, snapshot_idx, field_type)
-        resample(simulation_name, snapshot_idx, field_type)
+        if not get_resampled_delaunay_path(simulation_name, snapshot_idx, field_type).exists():
+            combine_snapshot(simulation_name, snapshot_idx, field_type)
+            run_delaunay(simulation_name, snapshot_idx, field_type)
+            resample(simulation_name, snapshot_idx, field_type)
 
-    if not get_delaunay_time_symlink_path(simulation_name, snapshot_idx, field_type).exists():
-        create_delaunay_symlink(simulation_name, snapshot_idx, field_type)
+        if not get_delaunay_time_symlink_path(simulation_name, snapshot_idx, field_type).exists():
+            create_delaunay_symlink(simulation_name, snapshot_idx, field_type)
 
-    if not get_resampled_delaunay_time_symlink_path(simulation_name, snapshot_idx, field_type).exists():
-        create_resampled_delaunay_symlink(simulation_name, snapshot_idx, field_type)
+        if not get_resampled_delaunay_time_symlink_path(simulation_name, snapshot_idx, field_type).exists():
+            create_resampled_delaunay_symlink(simulation_name, snapshot_idx, field_type)
+    except Exception as exc:
+        logger.exception("Failed job: %(job)s with exc: %(exc)s", {"job": simulation, "exc": exc})
+        raise exc from exc
 
 
 def cli() -> int:
