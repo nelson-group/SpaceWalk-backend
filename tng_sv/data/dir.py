df589e9d2f6a54d8db1a0f74e29c958b30f2c5f1
buermarc <buermarc2@gmail.com>
2022-11-28 10:21:39 +0100

    fix(symlinks): timeseries information for paraview

--- tng_sv/data/dir.py
+++ tng_sv/data/dir.py
@@ -53,7 +53,7 @@ def get_delaunay_time_symlink_path(simulation_name: str, snapshot_idx: int, fiel
     if not _dir.exists():
         os.makedirs(_dir)
 
-    return _dir.joinpath(f"combined_{field_type.value}_{snapshot_idx:03d}_delaunay.pvd")
+    return _dir.joinpath(f"combined_{field_type.value}_delaunay.{snapshot_idx:03d}.pvd")
 
 
 def get_resampled_delaunay_time_symlink_path(simulation_name: str, snapshot_idx: int, field_type: FieldType) -> Path:
@@ -62,4 +62,4 @@ def get_resampled_delaunay_time_symlink_path(simulation_name: str, snapshot_idx:
     if not _dir.exists():
         os.makedirs(_dir)
 
-    return _dir.joinpath(f"combined_{field_type.value}_{snapshot_idx:03d}_resampled_delaunay.pvd")
+    return _dir.joinpath(f"combined_{field_type.value}_resampled_delaunay.{snapshot_idx:03d}.pvd")
