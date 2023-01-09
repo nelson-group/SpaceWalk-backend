6b621e71376ee28a2d796f0409e802dbe6e23ef9
buermarc <buermarc2@gmail.com>
2023-01-09 13:42:22 +0100

    fix: adapt naming scheme for yt

--- tng_sv/api/download.py
+++ tng_sv/api/download.py
@@ -65,8 +65,7 @@ def download_subhalos(simulation_name: str, begin_snapshot: int, begin_idx: int,
             get_file(
                 cutout_url,
                 pre_dir=_dir,
-                post_fix="." + str(subhalo_meta["snap"]),
-                override_filename=f"cutout_{begin_snapshot}_{begin_idx}.hdf5",
+                override_filename=f"cutout_{begin_snapshot}_{begin_idx}.{subhalo_meta['snap']}.hdf5",
             )
             count += 1
         subhalo_meta = get_json(next_subhalo)
