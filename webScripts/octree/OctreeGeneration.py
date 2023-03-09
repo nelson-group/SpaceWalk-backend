8307b85fdd99b3a173650230cedf4069c5d0b55f
GimlisCode <johannes.maul@gmx.de>
2023-03-09 22:38:29 +0100

    safe possibility for orctrees init of load of octree

--- webScripts/octree/OctreeGeneration.py
+++ webScripts/octree/OctreeGeneration.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import open3d as o3d
 from tqdm import tqdm
+import pickle
 
 from tng_sv.webScripts.preprocessing.gridGeneration import getSameParticleInTwoDataSets
 
@@ -29,18 +30,48 @@ def generateOctree(coordinates, max_depth = 5):
 
 
 def preprocessSnaps(basePath, baseSnapId, fields, nSnapsToLoad, sizePerLeaf=100,safeOctree=False):
+    global idx
+
     loadHeader = True
     allLoadedSnaps = loadDatasets(basePath, baseSnapId, fields, nSnapsToLoad, loadHeader)
     maxDepth = np.ceil(np.log2(allLoadedSnaps[0]["snapInfo"]["BoxSize"] / sizePerLeaf)).astype(int) # approximation der tiefe bei sizePerLeaf Angabe => sizeLeaf ~ sizeBox / 2^x ==> formel vorne
+    print(maxDepth)
     allOctrees = list()
+
 
     for i in tqdm(range(nSnapsToLoad-1),total=nSnapsToLoad-1):
         allCombinedAttributes = getSameParticleInTwoDataSets(allLoadedSnaps[i]["snapData"], allLoadedSnaps[i+1]["snapData"], fields)
         coordinates = np.vstack(allCombinedAttributes['Coordinates'])
         # densities = np.hstack(allCombinedAttributes['Density'])
         octTree = generateOctree(coordinates, maxDepth)
+        indicesForOctree = []
+        idx = 0
+
+        def changeIdsWithListId(node, node_info):
+            if isinstance(node, o3d.geometry.OctreeLeafNode):
+                global idx
+                indicesForOctree.append(np.array(node.indices))
+                node.indices = [idx]
+                idx += 1
+                return True
+            if isinstance(node, o3d.geometry.OctreeInternalPointNode):
+                node.indices = []
+
+        octTree.traverse(changeIdsWithListId)
+        sum = 0
+        for idx, val in enumerate(indicesForOctree):
+            sum += len(val)
+        print(sum / len(indicesForOctree))
+
         if not safeOctree:
             allOctrees.append(octTree)
+        else:
+            file_name = basePath + "snapdir_" + str(baseSnapId + i).zfill(3)+"/o3dOctree.json"
+            print("Safe Octree to " + file_name)
+            if o3d.io.write_octree(file_name, octTree):
+                print(f'Object successfully saved to "{file_name}"')
+            else:
+                print(f'Object was not saved to "{file_name}"')
 
     return allOctrees
 
@@ -50,9 +81,9 @@ def main():
     baseSnapId = 75
     basePath = 'D:/VMShare/Documents/data/'
     fields = ['Coordinates', 'ParticleIDs']
-    nSnapsToLoad = 2
+    nSnapsToLoad = 10
 
-    preprocessSnaps(basePath, baseSnapId, fields, nSnapsToLoad, sizePerLeaf=100)
+    preprocessSnaps(basePath, baseSnapId, fields, nSnapsToLoad, sizePerLeaf=350, safeOctree=True)
 
 if __name__ == "__main__":
     main()
